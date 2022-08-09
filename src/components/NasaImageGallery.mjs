3fa28d4dfc8b51488d9b878b069b6394e04205bc
Dan Wells <dantheradarman@gmail.com>
2022-08-09 21:54:55 +0100

    Added NasaImageGallery

---
+++ src/components/NasaImageGallery.mjs
@@ -0,0 +1,43 @@
+// @ts-check
+
+import { getPictureOfTheDay } from "../api/nasa.mjs";
+import { createSlotContent } from "../helper.mjs";
+import { ImagePreview } from "./ImagePreview.mjs";
+
+class NasaImageGallery extends HTMLElement {
+  #grid;
+
+  constructor() {
+    super();
+
+    this.attachShadow({ mode: "open" });
+
+    this.#grid = document.createElement("grid-layout");
+
+    this.shadowRoot.append(this.#grid);
+
+    this.loadImages();
+  }
+
+  async loadImages(count=10) {
+    try {
+      const pictures = await getPictureOfTheDay(count);
+      pictures.forEach((picture) => {
+        const imagePreview = document.createElement("image-preview");
+        imagePreview.setAttribute("src", picture.url);
+        createSlotContent(imagePreview, "title", "p", picture.title);
+        createSlotContent(imagePreview, "description", "p", picture.explanation);
+        if (picture.copyright) {
+          createSlotContent(imagePreview, "addendum", "p", picture.copyright);
+        }
+
+        this.#grid.append(imagePreview);
+      })
+    } catch (e) {
+      // Error loading images
+      console.error("There was an error loading images");
+    }
+  }
+}
+
+window.customElements.define("nasa-image-gallery", NasaImageGallery);
