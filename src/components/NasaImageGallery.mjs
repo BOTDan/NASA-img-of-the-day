f1b6073bc88e7398c1ab915342590eddfde328dd
Dan Wells <dantheradarman@gmail.com>
2022-08-10 12:33:13 +0100

    Added error handling

--- src/components/NasaImageGallery.mjs
+++ src/components/NasaImageGallery.mjs
@@ -4,9 +4,15 @@ import { getPictureOfTheDay } from "../api/nasa.mjs";
 import { createSlotContent } from "../helper.mjs";
 import "./ImageCard.mjs";
 import "./LoadingButton.mjs";
+import "./ErrorCard.mjs";
 
+/**
+ * An image gallery displaying NASA pictures of the day
+ */
 class NasaImageGallery extends HTMLElement {
   #grid;
+  #error;
+  #errorDesc;
   #loadMore;
 
   constructor() {
@@ -18,8 +24,15 @@ class NasaImageGallery extends HTMLElement {
     wrapper.classList.add("nasa-image-gallery");
 
     this.#grid = document.createElement("div");
-    this.#grid.classList.add("nasa-image-gallery");
+    this.#grid.classList.add("grid");
     wrapper.append(this.#grid);
+
+    this.#error = document.createElement("error-card");
+    this.#error.setAttribute("hidden", "true");
+    wrapper.append(this.#error);
+
+    createSlotContent(this.#error, "title", "p", "There was an error loading images");
+    this.#errorDesc = createSlotContent(this.#error, "description", "p");
 
     this.#loadMore = document.createElement("loading-button");
     this.#loadMore.onclick = async () => {
@@ -37,8 +50,13 @@ class NasaImageGallery extends HTMLElement {
     this.loadImages();
   }
 
+  /**
+   * Loads a new set of images from the API and appends them to the list
+   * @param {number} [count] The amount of image to load
+   */
   async loadImages(count=10) {
-    this.#loadMore.setAttribute("disabled", "true");
+    this.setError(false);
+    this.#loadMore.setAttribute("loading", "true");
     try {
       const pictures = await getPictureOfTheDay(count);
       pictures.forEach((picture) => {
@@ -50,14 +68,27 @@ class NasaImageGallery extends HTMLElement {
         if (picture.copyright) {
           createSlotContent(imagePreview, "addendum", "p", picture.copyright);
         }
-
         this.#grid.append(imagePreview);
       })
     } catch (e) {
       // Error loading images
       console.error("There was an error loading images");
+      this.setError(e);
     }
-    this.#loadMore.removeAttribute("disabled");
+    this.#loadMore.removeAttribute("loading");
+  }
+
+  /**
+   * Sets if there's an error on this component
+   * @param {any} [error] The error
+   */
+  setError(error) {
+    if (!error) {
+      this.#error.setAttribute("hidden", "true");
+    } else {
+      this.#error.removeAttribute("hidden");
+      this.#errorDesc.textContent = `${error}`;
+    }
   }
 }
 
